b7f61a61b590899bb43beb528ee730837621082e
TheDawg101 <curtisgass@hotmail.com>
2022-10-11 14:55:39 -0400

    Confirm password function created

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let confirmPwd = document.getElementById("confirm-password");
-let pwd = document.getElementById("password").textContent;
+let pwd = document.getElementById("password");
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click", e => {
-    if (pwd === confirmPwd.textContent && confirmPwd.classList.contains("invalid")) {
-        confirmPwd.classList.remove("invalid");
+function passwordValidation () {
+    if (confirmPwd.value === "") {
+        submitBtn.disabled = false;
+    } else if (confirmPwd.value != pwd.value) {
+        submitBtn.disabled = true;
         confirmPwd.classList.remove("valid");
-    } else if (pwd != confirmPwd && confirmPwd.classList.contains("invalid")) {
-        //nothing
-    } else if (pwd != confirmPwd) {
         confirmPwd.classList.add("invalid");
-        confirmPwd.classList.remove("valid");
-    } else if (pwd === confirmPwd) {
+    } else {
+        submitBtn.disabled = false;
+        confirmPwd.classList.remove("invalid");
         confirmPwd.classList.add("valid");
     }
-})
+}
+
+pwd.addEventListener("keyup", passwordValidation);
+confirmPwd.addEventListener("keyup", passwordValidation);
